ea35556f9c286998a852fcf520ecdda20aebfe3c
papilio-machaon <ape03@479-mac-08.sep.cs.ox.ac.uk>
2023-04-24 14:45:22 +0100

    Test Commit

--- chalicelib/location_service.py
+++ chalicelib/location_service.py
@@ -3,5 +3,6 @@ def information_about(place):
         return "London is the capital and largest city of England and the United Kingdom"
     if place == "Oxford":
         return "Oxford is home to the University of Oxford, the oldest university in the English-speaking world"
-
+    if place == "Berlin":
+        return "Test Message XYZ"
     return f"Unknown place: {place}"
